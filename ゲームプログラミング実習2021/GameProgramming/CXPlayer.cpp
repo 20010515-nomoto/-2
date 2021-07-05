63b243ec45ccc61c3911dbe25d8c82e3ab25f8c8
野元　錬 <20010515@anabuki-net.ne.jp>
2021-07-05 12:23:17 +0900

    課題22完成

--- ゲームプログラミング実習2021/GameProgramming/CXPlayer.cpp
+++ ゲームプログラミング実習2021/GameProgramming/CXPlayer.cpp
@@ -5,10 +5,16 @@
 void CXPlayer::Update(){
 	if (CKey::Push('W')){
 		ChangeAnimation(1, true, 60);
-		mPosition += CVector(0.0f, 0.0f, 0.1f);
+		mPosition += CVector(0.0f, 0.0f, 0.1f) * mMatrixRotate;
 	}
 	else{
 		ChangeAnimation(0, true, 60);
+	}
+	if (CKey::Push('A')){
+		mRotation.mY += 2;
+	}
+	if (CKey::Push('D')){
+		mRotation.mY -= 2;
 	}
 	CXCharacter::Update();
 }
